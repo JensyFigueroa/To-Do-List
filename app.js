18f28e604303e233b195bfd57307a0caa554b405
Jensy Figueroa <jensy1418@gmail.com>
2023-03-28 19:48:38 -0400

    se pone en funcionamiento el btn Delete

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const formulario = document.getElementById('formulario');
 const input = document.querySelector('input')
-const listToDo = document.getElementById('listToDo');
-const template = document.getElementById('template');
+const boxList = document.getElementById('boxList');
+const template = document.getElementById('template').content;
 const fragment = document.createDocumentFragment();
 
 let collectionToDo = {};
 let id = 0;
+
+/* El DOMContentLoaded es por si hay algo en collectionToDo 
+lo muestre al momento de cargar el html */
+document.addEventListener('DOMContentLoaded', ()=>{
+    showToDo()
+})
 
 formulario.addEventListener('submit', e =>{
     e.preventDefault();
@@ -14,8 +20,11 @@ formulario.addEventListener('submit', e =>{
     console.log(e.target[0].value);
     console.log(e.target.querySelector('input').value)
     console.log(input.value) */
-
     addToDo(e)
+})
+
+boxList.addEventListener('click', (e) =>{
+    btnAction(e)
 })
 
 const addToDo = (e) =>{
@@ -29,7 +38,7 @@ const addToDo = (e) =>{
 
     const toDo = {
         id: ++id,
-        toDo: e.target[0].value,
+        toDo: input.value,
         state: false
     }
    
@@ -45,14 +54,34 @@ const addToDo = (e) =>{
 }
 
 const showToDo = () =>{
-    listToDo.innerHTML = '';
+    boxList.innerHTML = '';
     Object.values(collectionToDo).forEach( etoDo =>{
-        // console.log(toDo)
+        //  console.log(etoDo.toDo)
         const clone = template.cloneNode(true);
         // console.log(clone)
-        clone.querySelector('p').textContent = etoDo.toDo
+        clone.querySelector('p').textContent = etoDo.toDo;
+        clone.querySelectorAll('.check')[0].dataset.id = etoDo.toDo;
+        clone.querySelectorAll('.delete')[0].dataset.id = etoDo.toDo;
+        
         fragment.appendChild(clone)
     })
 
-    listToDo.appendChild(fragment)
+    boxList.append(fragment)
+}
+
+const btnAction = (e) => {
+//  console.log(e.target.classList.contains('check'))
+    if (e.target.classList.contains('check')) {
+        collectionToDo[e.target.dataset.id].state = true;
+        showToDo()
+        // console.log(collectionToDo)
+    }
+    if (e.target.classList.contains('delete')) {
+        delete collectionToDo[e.target.dataset.id]
+        showToDo()
+        // console.log(collectionToDo)
+    }
+
+    /* Activa solo los eventos que se encuentren dentro este contendor  */
+    e.stopPropagation()
 }
